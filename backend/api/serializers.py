3b2fecd4fb6f956b21db6f7f9fc4664ea8fcfd6a
fastmed <maks.memelige@gmail.com>
2022-03-11 18:28:11 +0200

    Add CaseSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from .models import Case, Event, Callback
+
+
+class CaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Case
+        fields = "__all__"
