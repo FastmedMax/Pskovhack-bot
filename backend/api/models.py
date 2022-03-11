ff5a693e8e744813a91af79923b35d90c8345ce9
fastmed <maks.memelige@gmail.com>
2022-03-11 18:26:47 +0200

    Add Case model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Case(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=60)
+    description = models.TextField(verbose_name="Описание")
