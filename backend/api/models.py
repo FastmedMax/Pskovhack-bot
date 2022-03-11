e3fed77e7caf75099a723720be6590e46a1c5ca1
fastmed <maks.memelige@gmail.com>
2022-03-11 18:27:21 +0200

    Add Callback model

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,3 +10,17 @@ class Case(models.Model):
 class Event(models.Model):
     title = models.CharField(verbose_name="Название", max_length=60)
     description = models.TextField(verbose_name="Описание")
+
+
+class Callback(models.Model):
+    class Type(models.TextChoices):
+        TELEGRAM = "Telegram", "Телеграм"
+        VK = "VK", "ВК"
+
+    name = models.CharField(verbose_name="Имя", max_length=30)
+    user_id = models.CharField(verbose_name="ID", max_length=50)
+    title = models.CharField(verbose_name="Название", max_length=60)
+    type = models.CharField(
+        verbose_name="Тип",
+        choices=Type.choices,
+        max_length=30)
