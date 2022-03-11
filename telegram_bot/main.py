869326e2fcf56fd52a78e9c7ea11836a07403fdd
fastmed <maks.memelige@gmail.com>
2022-03-11 23:48:27 +0200

    Init bot

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -8,3 +8,14 @@ from aiogram.dispatcher import FSMContext
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+URL = os.getenv("DJANGO_HOST")
+API_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
