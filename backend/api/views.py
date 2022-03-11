707d3d6474ada70322c4fe602be876c051600502
fastmed <maks.memelige@gmail.com>
2022-03-11 18:28:57 +0200

    Add CaseViewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,3 +8,15 @@ from .serializers import CaseSerializer, EventSerializer, CallbackSerializer
 
 
 # Create your views here.
+class CaseViewset(viewsets.GenericViewSet):
+    queryset = Case.objects.all()
+    serializer_class = CaseSerializer
+
+    def retrieve(self, request, pk=None):
+        case = self.get_object()
+        serializer = self.serializer_class(case)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
