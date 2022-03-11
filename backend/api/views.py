8bf79cf2dadb2f15986cd92f793cb902c86458da
fastmed <maks.memelige@gmail.com>
2022-03-11 18:28:46 +0200

    Add imports

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+
+from .models import Case, Event, Callback
+
+from .serializers import CaseSerializer, EventSerializer, CallbackSerializer
+
 
 # Create your views here.
