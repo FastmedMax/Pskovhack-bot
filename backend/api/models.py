23b8b6abb1b6434b99c82e437182bfa01c97227e
fastmed <maks.memelige@gmail.com>
2022-03-11 18:27:01 +0200

    Add Event model

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Case(models.Model):
     title = models.CharField(verbose_name="Название", max_length=60)
     description = models.TextField(verbose_name="Описание")
+
+
+class Event(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=60)
+    description = models.TextField(verbose_name="Описание")
