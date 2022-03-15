eb5fba4aed1759572a8e30b06d490b4f9fc37ee6
fastmed <maks.memelige@gmail.com>
2022-03-15 20:58:35 +0200

    Init bot

--- vk_bot/main.py
+++ vk_bot/main.py
@@ -5,3 +5,9 @@ from loguru import logger
 from vkbottle import Keyboard, Text, BaseStateGroup, TemplateElement, template_gen
 from vkbottle.bot import Bot, Message
 
+
+URL = os.getenv("DJANGO_HOST")
+bot = Bot(token=os.getenv("VK_TOKEN"))
+
+
+bot.run_forever()
