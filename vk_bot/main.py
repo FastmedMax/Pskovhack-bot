46c601c406a2b373bf7b7cb7e1c8c960b3cdb10c
fastmed <maks.memelige@gmail.com>
2022-03-15 20:59:35 +0200

    Add portfolio

--- vk_bot/main.py
+++ vk_bot/main.py
@@ -31,4 +31,39 @@ async def hi_handler(message: Message):
     )
     await message.answer("Привет, {}".format(users_info[0].first_name), keyboard=KEYBOARD_WITH_BUILDER)
 
+
+@bot.on.message(payload={"command":"portfolio"})
+async def portfolio(message: Message):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{URL}/api/cases/") as response:
+            if response.status == 200:
+                cases = await response.json()
+            else:
+                logger.error(await response.text())
+    
+    keyboard_1 = Keyboard().add(Text("button 1", {})).get_json()
+    list_elements = []
+    elements = []
+    for i, case in enumerate(cases):
+        if (i // 10) == 1:
+            list_elements.append(elements)
+            elements = []
+        
+        elements.append(TemplateElement(
+            buttons=keyboard_1,
+            title=case[1],
+            description="text"
+        ))
+
+    list_elements.append(elements)
+
+    text = "Список"
+
+    for element in list_elements:
+        template = template_gen(
+            *element
+        )
+        
+        await message.answer(message=text, template=template)
+
 bot.run_forever()
