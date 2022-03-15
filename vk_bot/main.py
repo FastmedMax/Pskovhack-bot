4c15e35fe3fc48fa35ff28da88b6a7e940d685b6
fastmed <maks.memelige@gmail.com>
2022-03-15 20:59:04 +0200

    Add start command

--- vk_bot/main.py
+++ vk_bot/main.py
@@ -15,4 +15,20 @@ class MenuState(BaseStateGroup):
     CONTACT = 2
     TEXT = 3
 
+
+@bot.on.message(text="Start")
+@bot.on.message(payload={"command":"start"})
+async def hi_handler(message: Message):
+    users_info = await bot.api.users.get(message.from_id)
+    KEYBOARD_WITH_BUILDER = (
+        Keyboard(one_time=False, inline=False)
+        .add(Text("Портфолио", payload={"command": "portfolio"}))
+        .add(Text("Мероприятия", payload={"command": "events"}))
+        .row()
+        .add(Text("О компании", payload={"command": "about"}))
+        .add(Text("Обратная связь", payload={"command": "callback"}))
+        .get_json()
+    )
+    await message.answer("Привет, {}".format(users_info[0].first_name), keyboard=KEYBOARD_WITH_BUILDER)
+
 bot.run_forever()
